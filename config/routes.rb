960a55abc4ca266f15c3d3eab3b9e60f5f1e8957
DDT1129 <dai1129dt@gmail.com>
2023-03-30 00:26:42 +0900

    空のメッセージが送信できないバリデーションの設定とチャットルームの削除機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'messages/index'
   root to: "rooms#index"
   resources :users, only: [:edit, :update]
-  resources :rooms, onluy: [:new, :create] do
+  resources :rooms, onluy: [:new, :create, :destroy] do
     resources :messages, only: [:index, :create]
   end
 
